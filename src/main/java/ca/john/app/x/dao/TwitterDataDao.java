576b0b2b2cecee45a39144ed2c906dc0b0aa8392
John Park <park.hokyun@gmail.com>
2024-12-30 01:01:35 -0500

    update

--- src/main/java/ca/john/app/x/dao/TwitterDataDao.java
+++ src/main/java/ca/john/app/x/dao/TwitterDataDao.java
@@ -5,6 +5,17 @@ import ca.john.app.x.model.TwitterData;
 
 public class TwitterDataDao implements CrdDao<TwitterData, String>{
 
+  // URI
+  private static final String API_URI = "https://api.x.com/2/tweets";
+
+  // URI symbols
+  private static final String QUERY_SYM = "?";
+  private static final String AMPERSAND = "&";
+  private static final String EQUAL = "=";
+
+  // Response code
+  private static final int HTTP_OK = 200;
+
   @Override
   public TwitterData create(TwitterData entity) {
     return null;
